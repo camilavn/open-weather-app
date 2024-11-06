4edddf32d9c6f6acfb7ab063ab2011dc2df68cd5
Naser Al Madi <nsalmadi@colby.edu>
2024-11-06 01:01:38 -0500

    adding env and reqs for deplyment to Heroku with db

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from models import db, User
 from flask_login import LoginManager
 from views import main_blueprint
 from auth import auth_blueprint
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret keyyyyy'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres", "postgresql", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
